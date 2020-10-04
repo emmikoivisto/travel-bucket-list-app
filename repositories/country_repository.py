d8fe3665cef6c17a635633c953f1d6dfc2752dc5
emmikoivisto <emetsi@gmail.com>
2020-10-04 10:23:55 +0100

    country update function added

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -45,3 +45,15 @@ def update(country):
     sql = "UPDATE countries SET VALUES (name) = (%s) WHERE id = %s"
     values = [country.name, country.id]
     run_sql(sql, values)
+
+def cities(country):
+    cities = []
+
+    sql = "SELECT * FROM cities WHERE country_id = %s"
+    values = [country.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        city = City(row['name'], row['country_id'], row['visited'], row['id'])
+        cities.append(city)
+    return cities
